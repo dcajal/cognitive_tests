d720b3a25664ffea59de0e67fff3db0d2e03cbbe
dcajal <dcajal95@gmail.com>
2025-08-07 15:25:45 +0200

    Add Stroop PDF viewer widget and related assets; update pubspec.yaml for dependencies

--- lib/cognitive_tests.dart
+++ lib/cognitive_tests.dart
@@ -2,3 +2,4 @@ export 'src/tests/stroop_test.dart';
 export 'src/tests/trail_making_test.dart';
 export 'src/interfaces/test_result_handler.dart';
 export 'src/models/test_results.dart';
+export 'src/widgets/stroop_pdf_viewer.dart';
---
+++ lib/src/widgets/stroop_pdf_viewer.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+
+/// Widget that handles PDF viewing for cognitive tests
+/// This widget should be moved to the cognitive_tests package
+class StroopPdfViewer extends StatefulWidget {
+  /// Path to the PDF asset
+  final String assetPath = 'assets/stress_tests/stroop.pdf';
+
+  /// Current page number to display
+  final int currentPage;
+
+  /// Callback when page changes are needed
+  final VoidCallback? onPageChangeRequested;
+
+  const StroopPdfViewer({
+    super.key,
+    required this.currentPage,
+    this.onPageChangeRequested,
+  });
+
+  @override
+  State<StroopPdfViewer> createState() => _StroopPdfViewerState();
+}
+
+class _StroopPdfViewerState extends State<StroopPdfViewer> {
+  late final PdfViewerController _pdfController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pdfController = PdfViewerController();
+  }
+
+  @override
+  void didUpdateWidget(StroopPdfViewer oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    // Update PDF page when currentPage changes
+    if (oldWidget.currentPage != widget.currentPage) {
+      _pdfController.jumpToPage(widget.currentPage);
+    }
+  }
+
+  @override
+  void dispose() {
+    _pdfController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SfPdfViewer.asset(
+      widget.assetPath,
+      controller: _pdfController,
+      pageLayoutMode: PdfPageLayoutMode.single,
+      canShowScrollHead: false,
+      enableDoubleTapZooming: false,
+      enableTextSelection: false,
+      enableDocumentLinkAnnotation: false,
+    );
+  }
+
+  /// Get the PDF controller for external access if needed
+  PdfViewerController get controller => _pdfController;
+}
